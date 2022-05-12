c903122a0e0d70c34473aa30e5c258af9416373c
Wen-Lin Lee <82gogolee@gmail.com>
2022-05-12 10:59:52 +0800

    adjust styles

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function showMovies(movies) {
   <span class="${getClassByRate(vote_average)}">${vote_average}</span>
 </div>
 <div class="overview">
-  <h3>Overview</h3>
+
 ${overview}
 </div>
 `;
